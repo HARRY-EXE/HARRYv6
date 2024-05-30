bffb22659d3c782d0c1fb7a8785b31bd6c434075
The Harry :P <94730463+HARRY-EXE@users.noreply.github.com>
2024-05-31 01:01:01 +0545

    Update run.py

--- run.py
+++ run.py
@@ -17,9 +17,14 @@ def main():
     global arch
     print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m CHECKING FOR UPDATES")
     os.system("git pull --quiet")
-    print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
-    print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
-    os.system("python3 data/HARRYv6-PyProtected-xD.so")
+    if arch = "32BIT":
+        print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
+        import HARRYv6_32
+        print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
+    elif arch = "64BIT":
+        print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
+        import HARRYv6_64
+        print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
 if __name__ == "__main__":
     check_python_architecture()
     main()
