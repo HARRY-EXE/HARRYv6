ad8ba339e3af457ce0785f688e83a91e78caec02
The Harry :P <94730463+HARRY-EXE@users.noreply.github.com>
2024-05-31 03:11:49 +0545

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import platform,os
-bit = platform.architecture()[0]
-print(' CHECKING FOR UPDATES');os.system('git pull')
-if bit == '32bit':
-    import HARRY32
-elif bit == '64bit':
-    import HARRY64
-else:exit()
