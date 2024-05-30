ace11ddf1a034fd4e31471c2baed49d008eecc32
The Harry :P <94730463+HARRY-EXE@users.noreply.github.com>
2024-05-31 03:36:07 +0545

    Update run.py

--- run.py
+++ run.py
@@ -9,8 +9,10 @@ def check_python_architecture():
     global arch
     architecture = platform.architecture()
     if architecture[0] == '32bit':
+        file = "HARRY32.cpython-311.so
         arch = "32BIT"
     elif architecture[0] == '64bit':
+        file = "HARRY64.cpython-311.so
         arch = "64BIT"
     else:
         arch = "INVALID"
@@ -22,11 +24,11 @@ def main():
     if arch == "32BIT":
         print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
         print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
-        import_(f'{login} data/HARRY32')
+        import_(f'{login} data/{file}')
     elif arch == "64BIT":
         print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
         print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
-        import_(f'{login} data/HARRY64')
+        import_(f'{login} data/{file}')
 if __name__ == "__main__":
     check_python_architecture()
     main()
