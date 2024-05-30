7f266ccb5ff034bbb0dc7c0ace785d21d50102cf
The Harry :P <94730463+HARRY-EXE@users.noreply.github.com>
2024-05-31 02:19:27 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,30 +1,8 @@
-import platform
-import os
-
-global arch
-
-def check_python_architecture():
-    global arch
-    architecture = platform.architecture()
-    if architecture[0] == '32bit':
-        arch = "32BIT"
-    elif architecture[0] == '64bit':
-        arch = "64BIT"
-    else:
-        arch = "INVALID"
-
-def main():
-    global arch
-    print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m CHECKING FOR UPDATES")
-    os.system("git pull --quiet")
-    if arch == "32BIT":
-        print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
-        print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
-        import HARRY32
-    elif arch == "64BIT":
-        print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
-        print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
-        import HARRY64
-if __name__ == "__main__":
-    check_python_architecture()
-    main()
+import platform,os
+bit = platform.architecture()[0]
+print(' CHECKING FOR UPDATES');os.system('git pull')
+if bit == '32bit':
+    import HARRY32
+elif bit == '64bit':
+    import HARRY64
+else: exit(" SORRY YOUR DEVICE DOESN'T SUPPORT THIS TOOL USE 32/64 BIT PHONE");exit()
