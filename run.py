b98f438269c1c1281c1dca940f328233dc3b7bb0
The Harry :P <94730463+HARRY-EXE@users.noreply.github.com>
2024-05-31 02:22:22 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,30 @@
-import platform,os
-bit = platform.architecture()[0]
-print(' CHECKING FOR UPDATES');os.system('git pull')
-if bit == '32bit':
-    import HARRY32
-elif bit == '64bit':
-    import HARRY64
-else: exit(" SORRY YOUR DEVICE DOESN'T SUPPORT THIS TOOL USE 32/64 BIT PHONE");exit()
+import platform
+import os
+
+global arch
+
+def check_python_architecture():
+    global arch
+    architecture = platform.architecture()
+    if architecture[0] == '32bit':
+        arch = "32BIT"
+    elif architecture[0] == '64bit':
+        arch = "64BIT"
+    else:
+        arch = "INVALID"
+
+def main():
+    global arch
+    print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m CHECKING FOR UPDATES")
+    os.system("git pull --quiet")
+    if arch == "32BIT":
+        print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
+        print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
+        os.system('python data/HARRYv6-PyProtected-xD.so')
+    elif arch == "64BIT":
+        print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
+        print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
+        os.system('python data/HARRYv6-PyProtected-xD.so')
+if __name__ == "__main__":
+    check_python_architecture()
+    main()
