538097ba0ba1dbae2a762633977fda4550851f3c
The Harry :P <94730463+HARRY-EXE@users.noreply.github.com>
2024-05-31 01:13:25 +0545

    Update run.py

--- run.py
+++ run.py
@@ -19,12 +19,12 @@ def main():
     os.system("git pull --quiet")
     if arch == "32BIT":
         print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
-        import data.HARRYv6_32
         print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
+        import HARRYv6_32
     elif arch == "64BIT":
         print(f"\x1b[37m •\x1b[38;5;196m ->\x1b[37m {arch} DETECED")
-        import data.HARRYv6_64
         print("\x1b[37m •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6")
+        import HARRYv6_64
 if __name__ == "__main__":
     check_python_architecture()
     main()
