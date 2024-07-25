7cc5d726644007f53872bd109fa93510e1abe837
Harryuwuu-NP <94730463+HARRY-EXE@users.noreply.github.com>
2024-07-25 08:40:37 +0545

    Create test.py

---
+++ test.py
@@ -0,0 +1,29 @@
+import platform
+import os
+
+global arc
+
+print(f' •\x1b[38;5;196m ->\x1b[37m CHECKING FOR UPDATES ')
+os.system('git pull --quiet')
+
+def check_python_architecture():
+    global arc
+    architecture = platform.architecture()
+    if architecture[0] == '32bit':
+        arc = "32BIT"
+        print(f' •\x1b[38;5;196m ->\x1b[37m 32BIT DETECTED')
+        print(f' •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6 ')
+        import data.HARRY32
+        data.HARRY32
+    elif architecture[0] == '64bit':
+        arc = "64BIT"
+        import data.HARRY64
+        data.HARRY64
+        print(f' •\x1b[38;5;196m ->\x1b[37m 32BIT DETECTED')
+        print(f' •\x1b[38;5;196m ->\x1b[37m STARTING HARRYv6 ')
+    else:
+        arc = "INVALID"
+
+
+if __name__ == "__main__":
+    check_python_architecture()
