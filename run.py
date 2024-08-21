6b5aec8c62033b621b6bb0cb0099283964f97956
Harryuwuu-NP <94730463+HARRY-EXE@users.noreply.github.com>
2024-08-21 15:27:06 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,30 @@
 import platform
 import os
+import socket
 
 global arc
 
 print(f' •\x1b[38;5;196m ->\x1b[37m CHECKING FOR UPDATES ')
 os.system('git pull --quiet')
 
+def check_internet():
+    try:
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError:
+        return False
+if check_internet():
+    pass
+else:
+    exit("•\x1b[38;5;196m ->\x1b[37m NO INTERNET CONNECTION")
+
+def check_storage_permission():
+    if os.path.isdir(os.path.expanduser("~") + "/storage"):
+        pass
+    else:
+        subprocess.run(["termux-setup-storage"], check=True)
+        exit("•\x1b[38;5;196m ->\x1b[37m NO STORAGE PERMISSION")
+        
 def check_python_architecture():
     global arc
     architecture = platform.architecture()
@@ -23,7 +42,10 @@ def check_python_architecture():
         data.HARRY64
     else:
         arc = "INVALID"
+        exit("•\x1b[38;5;196m ->\x1b[37m UNKNOWN DEVICE TYPE")
 
 
 if __name__ == "__main__":
+    check_internet()
+    check_storage_permission()
     check_python_architecture()
